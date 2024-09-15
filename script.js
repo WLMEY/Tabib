b894153309afa3b2db5ea4d318667893e812f55f
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2024-09-15 05:05:34 +0300

    ...

--- script.js
+++ script.js
@@ -4,6 +4,50 @@
 // ctx.lineTo(30, 100);
 // ctx.stroke();
 
+// const continer_FAQ=document.getElementById("continer_FAQ");
+// const icons=document.getElementById("icons")
+// const icon=icons.getElementsByTagName("i")
 
 
 
+// const continer_large =
+// `    <div class="continer_large" id="continer_large">
+//         <p>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, dolore.
+//             Sed vel aliquid officia optio, asperiores aspernatur error sapiente
+//             recusandae nulla ut ab quaerat placeat harum debitis, dolorem esse at
+//             nobis aliquam dolores exercitationem velit inventore, qui perferendis?
+//             Quaerat perferendis beatae labore optio repellat iste corporis voluptatem
+//             enim non. Voluptate.
+//         </p>
+//     </div>`
+
+
+//     for(let x=0 ;x<icon.length; x++){
+//         icon[x].onclick=change;
+//         icon[x].style.color="red";
+//         function change() {
+//             for(let x=0 ;x<icon.length; x++){
+//                 icon.onmousemove=()=>{icon[x].style.cursor="pointer"}
+//                 if (icon[x].className==="fa-solid fa-chevron-up"){
+//                     icon[x].className="fa-solid fa-chevron-down";
+//                 }
+//                 else{
+//                     icon[x].className="fa-solid fa-chevron-up";
+//                 }
+//             }
+//         }
+//     }
+
+
+////////////////////////////////////
+// const continer_FAQ=document.getElementById("continer_FAQ");
+// const item=document.getElementById("item")
+// const icon=item.getElementsByTagName("i")
+
+const items=document.querySelectorAll("item")
+items.forEach(item => {
+    // const question=item.querySelector(".")
+    const answer = item.querySelector('.answer');
+    const icon = item.querySelector('.icon');
+});
--- tabib_react/src/App.js
+++ tabib_react/src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './style.css';
+import './all.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      {/* Start codeing in 2024/9/11 03:00 AM */}
+      {/* Day 1 */}
+      <header className="nav_bar">
+        <div className="title">
+          <h1><span>T</span>abilb</h1>
+        </div>
+        <div className="titles">
+          <span><a href="#Home">Home</a></span>
+          <span><a href="#Services">Services</a></span>
+          <span><a href="#Works">How it works</a></span>
+          <span><a href="#Testimonials">Testimonials</a></span>
+          <span><a href="#FAQ_s">FAQ's</a></span>
+          <span className="Download"><a href="#Download_P">Download</a></span>
+          <div className="circle">
+            <div className="circle2"></div>
+          </div>
+        </div>
       </header>
+
+      {/* Content */}
+      <div className="content">
+        {/* Home */}
+        <section id="Home" className="Home">
+          <div className="left">
+            <h1>
+              Transforming <br />
+              <span>Healthcare</span> at Your <br />
+              Fingertips
+            </h1>
+            <p>
+              We are here for your care. Your Health is our first priority. Access <br />
+              Clinics, Doctors and get your Medicines easily anytime, anywhere <br />
+              with Tabib app. <br />
+            </p>
+            <div className="btn_home">
+              <button className="Download">Download Now</button>
+              <button className="btn_inverse">Learn more</button>
+            </div>
+            <div className="end_home">
+              <div className="container">
+                <div className="circle_home"><i className="fa-regular fa-hospital"></i></div>
+                <div className="right_container">
+                  <h4>400+</h4>
+                  <span>Clinics</span>
+                </div>
+              </div>
+              <div className="container">
+                <div className="circle_home"><i className="fa-solid fa-user-doctor"></i></div>
+                <div className="right_container">
+                  <h4>1K+</h4>
+                  <span>Doctors</span>
+                </div>
+              </div>
+              <div className="container">
+                <div className="circle_home"><i className="fa-solid fa-users"></i></div>
+                <div className="right_container">
+                  <h4>50K+</h4>
+                  <span>Users</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <img src="./5ba1a398ac0aa7fe01480166fd2b818f.png" alt="app preview" />
+          </div>
+        </section>
+
+        {/* Services */}
+        <section className="Services" id="Services">
+          <h3>Services</h3>
+          <h1>Why Choose Tabib</h1>
+          <div className="containers_s">
+            <div className="container_style">
+              <div className="circle_home"><i className="fa-solid fa-house-circle-check"></i></div>
+              <h5>Book an Appointment</h5>
+              <span>
+                Choose your suitable doctor and book <br />
+                appointments with just a few taps
+              </span>
+            </div>
+            <div className="container_style">
+              <div className="circle_home"><i className="fa-solid fa-user-tie"></i></div>
+              <h5>Teleconsultation</h5>
+              <span>
+                Consult with doctors from the comfort <br />
+                of your home
+              </span>
+            </div>
+            <div className="container_style">
+              <div className="circle_home"><i className="fa-solid fa-staff-snake"></i></div>
+              <h5>Purchase Medicines</h5>
+              <span>
+                Buy all your medical products easily <br />
+                from your nearest pharmacy
+              </span>
+            </div>
+            <div className="container_style">
+              <div className="circle_home"><i className="fa-solid fa-shield"></i></div>
+              <h5>Health insurance</h5>
+              <span>
+                Use your health insurance to purchase <br />
+                Medical products and book <br />
+                appointments
+              </span>
+            </div>
+          </div>
+        </section>
+
+        {/* Other sections like Works, Testimonials, Download, FAQ, and Footer can be similarly converted */}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
