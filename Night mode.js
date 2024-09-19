1bc9215de1f2a9ae7eb001c92ed801e460687899
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2024-09-19 08:54:42 +0300

    i love var(--love)

--- Night mode.js
+++ Night mode.js
@@ -1,30 +1,41 @@
 
 
 
-const body =document.querySelector('body')
-const nav_bar=document.querySelector('.nav_bar')
-const title =document.querySelector('.title')
+// const body =document.querySelector('body')
+// const nav_bar=document.querySelector('.nav_bar')
+// const title =document.querySelector('.title')
 
-var x=1;
+// var x=1;
+// function colors(){
+//     // body.style.backgroundColor="#232323"
+//     x++;
+//     if(x%2==0){
+//         black_color();
+//         console.log("black")
+//     }
+//     else{
+//         default_color();
+//         console.log("default")
+
+//     }
+
+// }
+// function black_color(){
+//     body.style.backgroundColor="#232323"
+
+// }
+// function default_color(){
+//     body.style.backgroundColor="#ffffff"
+
+// // }
+
+// :root{
+//     --body1-color:white;
+//     --body2-color:#f1f0f0;
+//     --iconsandtitles-color:#56C8B3;
+//     --hover-color:#3f9686;
+//     --text-color:#999999 ;
+// }
 function colors(){
-    // body.style.backgroundColor="#232323"
-    x++;
-    if(x%2==0){
-        black_color();
-        console.log("black")
-    }
-    else{
-        default_color();
-        console.log("default")
-
-    }
-
-}
-function black_color(){
-    body.style.backgroundColor="#232323"
-
-}
-function default_color(){
-    body.style.backgroundColor="#ffffff"
-
+    document.body.classList.toggle("NightMOde")
 }
